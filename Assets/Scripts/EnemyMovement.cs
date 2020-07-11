0ffd5f5d9e8c2424f07e0818b491ffee8ea700cd
Geogeo357 <geogeo357@gmail.com>
2020-07-10 22:06:33 -0500

    Level 1 working spawn and movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    GameObject[] pathNodes;
+    int nextIndex = 0;
+    Vector3 nextDestination;
+    public float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pathNodes = GameObject.Find("MapPath").GetComponent<MapPath>().pathNodes;
+        nextDestination = pathNodes[0].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((nextDestination - transform.position).sqrMagnitude < 0.1)
+        {
+            if (nextIndex < pathNodes.Length - 1)
+            {
+                nextIndex++;
+                nextDestination = pathNodes[nextIndex].transform.position;
+            }
+        }
+        else
+        {
+            transform.Translate((nextDestination - transform.position).normalized * movementSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject[] spawnpoints;
+    public float spawntime;
+    public GameObject enemy;
+
+    float timeSinceLastSpawn = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(timeSinceLastSpawn);
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn > spawntime)
+        {
+            Debug.Log("hm");
+
+            timeSinceLastSpawn = 0.0f;
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        GameObject.Instantiate(enemy, spawnpoints[0].transform.position, spawnpoints[0].transform.rotation);
+    }
+}
