24fa6f56bf2e865cd684798b5cb731dcdaf4fee5
Qwerty Bobberson <cebrhim@gmail.com>
2020-07-10 20:48:27 -0500

    Created Unity Project

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    [SerializeField] Texture2D map;
+    [SerializeField] ColorToObjectMap colorToObjectMap;
+
+    private void Start()
+    {
+        for(int x = 0; x < map.width; x++)
+        {
+            for(int y = 0; y < map.height; y++)
+            {
+            }
+        }
+    }
+}
